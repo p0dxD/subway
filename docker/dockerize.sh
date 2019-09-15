5827661311f4c4071345afdd2c599f0d9f595359
Jose Rodriguez <jose0797@gmail.com>
2019-09-15 13:03:40 -0400

    Automated creating of container

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # docker="$DOCKER"/docker.exe
-echo ""
-which docker
+echo "Creating image"
 docker build -t go-subway -f docker/Dockerfile .
+echo "Cealing old images"
+yes | docker image prune
+
+echo "Stopping old container"
+old_container=$(docker ps -a | grep "go-subway" | awk '{ print $1 }')
+if [ $old_container != "" ] then
+    docker stop $old_container
+    docker rm  $old_container
+fi
+
+echo "Starting new container"
+docker run -d -it -p 3001:3001 go-subway
