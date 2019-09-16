9cd75407d54ace70097ec5642bda9dbd0338aae3
Jose Rodriguez <jose0797@gmail.com>
2019-09-15 23:21:14 -0400

    Names libs object

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
         "log"
         "net/http"
-        L "./pkg/libs"
+        Libs "./pkg/libs"
 )
 
 
 func main() {
-        L.Init()
+        Libs.Init()
         http.Handle("/", http.FileServer(http.Dir("./static")))
         log.Print("Now serving: http://localhost:3001")
         http.ListenAndServe(":3001", nil)
