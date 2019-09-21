6846179f36c06de9696912726c0052faa27fe2c9
Jose Rodriguez <jose0797@gmail.com>
2019-09-21 13:49:49 -0400

    Testing

--- pkg/libs/stations.go
+++ pkg/libs/stations.go
@@ -99,7 +99,7 @@ func refreshStations(stations []string){
                 fmt.Printf("Inserting station:%+v\n",f.Properties)
                 fmt.Printf("Inserting station:%s\n",f.Properties["line"])
                 if stations != nil {
-                if strings.Contains(f.Properties["line"].(string), "1")  {
+                if strings.Contains(f.Properties["line"].(string), "1") || strings.Contains(f.Properties["line"].(string), "2")  {
                         Stations.Insert(&Station{f})
                 }
                 }else {
