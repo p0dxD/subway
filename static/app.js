38371e03101efb8959e55be30ae4c255bb732fe4
Jose Rodriguez <jose0797@gmail.com>
2019-09-21 12:09:47 -0400

    Log testing.

--- static/app.js
+++ static/app.js
@@ -115,7 +115,7 @@ const routeColors = {
 var mapRef
 // initMap is called from the Google Maps JS library after the library has initialised itself.
 function initMap() {
-  console.log("Initiating map")
+  console.log("Initiating map.")
   const map = new google.maps.Map(document.querySelector('#map'), {
     zoom: 12,
     center: {
